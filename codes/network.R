cf822835b03f4ad6ae867092944677e043bf9f8f
HoxoMaxwell <Greenwind120170@gmail.com>
2018-12-18 12:16:22 +0900

    .

--- codes/network.R
+++ codes/network.R
@@ -22,3 +22,44 @@ vcount(test.g)
 ecount(test.g)
 neighbors(test.g, 'A')
 mean(degree(test.g))
+
+graph.density(test.g)
+closeness(test.g)
+betweenness(test.g)
+page.rank(test.g)
+bonpow(test.g)
+transitivity(test.g)
+transitivity(test.g, type = 'local')
+assortativity.degree(test.g)
+
+library(igraphdata)
+library(gridExtra)
+data("foodwebs")
+
+food.df <- data.frame(
+  'ID' = c(1:3), 
+  'v' = rep(NaN, 3),
+  'e' = rep(NaN, 3), 
+  'd_mean' = rep(NaN, 3),
+  'density' = rep(NaN, 3),
+  'close_mean' = rep(NaN, 3)
+  )
+
+op <- par(mfrow = c(1, 3))
+for (i in 1:3) {
+  plot(
+    foodwebs[[i]],
+    layout = layout.kamada.kawai,
+    vertex.size = 2,
+    edge.arrow.size = 0.1,
+    vertex.label.color = 'deeppink',
+    vertex.label.cex = 0.9
+  )
+  food.df['v'][i, ] <- vcount(foodwebs[[i]])
+  food.df['e'][i, ] <- ecount(foodwebs[[i]])
+  food.df['d_mean'][i, ] <- mean(degree(foodwebs[[i]]))
+  food.df['density'][i, ] <- graph.density(foodwebs[[i]])
+  # food.df['close_mean'][i, ] <- mean(closeness(foodwebs[[i]]))
+}
+food.df
+par(op)
