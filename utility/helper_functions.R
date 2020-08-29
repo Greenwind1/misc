976984e81c39abd0dcaa9b23405f037eda3bb912
Maxwell <Greenwind120170@gmail.com>
2020-08-29 09:49:53 +0900

    .

---
+++ utility/helper_functions.R
@@ -0,0 +1,39 @@
+library(stringr)
+library(DT)
+
+
+
+ymd.conv <- function(date.code) {
+    if (is.na(date.code)) return(NA)
+    if (str_sub(date.code, end = 1) == 3) {
+        yr <- as.numeric(str_sub(date.code, start = 2, end = 3)) + 1925
+        md <- str_sub(date.code, start = 4, end = 7)
+        return(paste0(as.character(yr), md))
+        # return(as.POSIXct(paste0(as.character(yr), md), format = '%Y%m%d'))
+    }
+    else if (str_sub(date.code, end = 1) == 4){
+        yr <- as.numeric(str_sub(date.code, start = 2, end = 3)) + 1988
+        md <- str_sub(date.code, start = 4, end = 7)
+        return(paste0(as.character(yr), md))
+        # return(as.POSIXct(paste0(as.character(yr), md), format = '%Y%m%d'))
+    }
+    else return(NA)
+}
+vect.ymd.conv <- Vectorize(ymd.conv)
+
+
+view.table <- function(df, head.n = 100, pagelen = 10) {
+    datatable(
+        df[1:head.n, ], 
+        options = list(
+            pageLength = pagelen,
+            autoWidth = TRUE,
+            scrollX = TRUE,
+            columnDefs = list(list(
+                width = '175px',
+                targets = seq(1, dim(df)[2]))
+                )
+            ), 
+        class = 'cell-border stripe'
+    )
+}
