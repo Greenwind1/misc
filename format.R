e63cf2915ad61fde895596722eec812da6e00137
Maxwell <Greenwind120170@gmail.com>
2021-02-27 00:51:28 +0900

    .

---
+++ format.R
@@ -0,0 +1,36 @@
+# Data manipulation
+library(tidyverse)
+library(data.table)
+library(DT)
+library(stringr)
+library(lubridate)
+
+# Visualisation
+library(ggplot2)
+library(patchwork)
+library(broman)
+library(gtsummary)
+library(summarytools)
+library(leaflet)  # map
+
+# Interactivity
+library(plotly)
+library(crosstalk)
+
+# Parallel
+library(purrr)
+library(foreach)
+library(doParallel)
+
+# Statiscis Analysis
+library(psych)
+library(naniar)
+library(lme4)
+library(lmerTest)
+library(coefplot)
+
+library(rstan)
+library(bayesplot)
+
+library(broom)
+library(broom.mixed)
