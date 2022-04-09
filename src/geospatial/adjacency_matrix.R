36e654608e484f9ad8edfbb5b536178bdf33e759
Maxwell <Greenwind120170@gmail.com>
2022-04-10 00:44:11 +0900

    auto git

---
+++ src/geospatial/adjacency_matrix.R
@@ -0,0 +1,46 @@
+# IWANAMI DATA SCIENCE Vol.4
+# pp.62-63
+# Adjacency matrix for CAR model
+
+library(sf)
+library(spdep)
+library(maptools)
+library(rmapshaper)
+library(tidyverse)
+library(stringr)
+library(extrafont)  # "Candara"
+
+theme_set(theme_minimal(base_family = "Candara"))
+options(dplyr.summarise.inform = TRUE)
+
+map.sf <- sf::read_sf(
+    "input/Administrative_district_Tokyo_2021/N03-21_13_210101.shp"
+    # "input/Administrative_district_Shizuoka_2021/N03-21_22_210101.shp"
+)
+map.sp <- maptools::readShapeSpatial(
+    "input/Administrative_district_Tokyo_2021/N03-21_13_210101.shp"
+)
+
+map.sf %>% 
+    filter(str_detect(N03_004, "区")) %>% 
+    # rmapshaper::ms_filter_islands(min_area = 1e8) %>% 
+    ggplot() + 
+    geom_sf()
+
+# A neighbours list with class nb. See card for details of “nb” objects.
+map.sf.nb <- spdep::poly2nb(map.sf)
+map.sp.nb <- spdep::poly2nb(map.sp)
+
+summary(map.sf.nb)
+summary(map.sp.nb)
+
+num <- rep(NA, length(map.sp))
+
+for (i in 1:length(map.sp)) {
+    num[i] <- length(map.sp.nb[[i]])
+}
+
+adj <- unlist(map.sp.nb)
+
+# cat(num)
+# cat(adj)
