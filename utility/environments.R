49ee676605b2dd6d1dcb51ed553d0fbbdba299c3
Maxwell <Greenwind120170@gmail.com>
2022-07-23 01:20:12 +0900

    auto git

--- utility/environments.R
+++ utility/environments.R
@@ -1,29 +1,30 @@
 library(ggplot2)
 
 # colors ----
-col.tw <- rgb(219, 215, 210, maxColorValue = 255)  # TimberWolf
-col.os <- rgb(65, 74, 76, maxColorValue = 255)     # Outer Space
-col.rp <- rgb(120, 81, 169, maxColorValue = 255)   # Royal Purple
-col.mb <- rgb(26, 72, 118, maxColorValue = 255)    # Midnight Blue
-col.pb <- rgb(28, 169, 201, maxColorValue = 255)   # Pacific Blue
-col.cb <- rgb(176, 183, 198, maxColorValue = 255)  # Cadet Blue
-col.el <- rgb(206, 255, 29, maxColorValue = 255)   # Electric Lime
-col.cg <- rgb(0, 204, 153, maxColorValue = 255)    # Caribbean Green
-col.mt <- rgb(255, 130, 67, maxColorValue = 255)   # Mango Tango
-col.rm <- rgb(227, 37, 107, maxColorValue = 255)   # RazzMatazz
-col.sl <- rgb(252, 40, 71, maxColorValue = 255)    # ScarLet
+col.tw <- rgb(219, 215, 210, maxColorValue = 255)   # TimberWolf
+col.os <- rgb(65, 74, 76, maxColorValue = 255)      # Outer Space
+col.r.p <- rgb(120, 81, 169, maxColorValue = 255)   # Royal Purple
+col.m.b <- rgb(26, 72, 118, maxColorValue = 255)    # Midnight Blue
+col.p.b <- rgb(28, 169, 201, maxColorValue = 255)   # Pacific Blue
+col.c.b <- rgb(176, 183, 198, maxColorValue = 255)  # Cadet Blue
+col.el <- rgb(206, 255, 29, maxColorValue = 255)    # Electric Lime
+col.c.g <- rgb(0, 204, 153, maxColorValue = 255)    # Caribbean Green
+col.mt <- rgb(255, 130, 67, maxColorValue = 255)    # Mango Tango
+col.rm <- rgb(227, 37, 107, maxColorValue = 255)    # RazzMatazz
+col.sl <- rgb(252, 40, 71, maxColorValue = 255)     # ScarLet
 col.plos.yellow <- rgb(214, 225, 61, maxColorValue = 255)   # PLOS Yellow
 col.plos.pink <- rgb(207, 0, 163, maxColorValue = 255)      # PLOS Pink
-col.bmc.gray <- rgb(255, 253, 246, maxColorValue = 255)   # BMC Gray
-col.bmc.purple <- rgb(191, 18, 248, maxColorValue = 255)  # BMC Purple
-col.bmc.navy <- rgb(26, 46, 79, maxColorValue = 255)      # BMC Navy Blue
-col.bmc.blue <- rgb(1, 74, 129, maxColorValue = 255)      # BMC Blue
-col.bmc.sky <- rgb(15, 153, 190, maxColorValue = 255)     # BMC Sky Blue
-col.bmc.green <- rgb(0, 73, 64, maxColorValue = 255)      # BMC Deep Green
-col.bmc.pink <- rgb(239, 37, 95, maxColorValue = 255)     # BMC Pink
+col.bmc.gray.p <- rgb(255, 253, 246, maxColorValue = 255)   # BMC Pale Gray
+col.bmc.purple <- rgb(191, 18, 248, maxColorValue = 255)    # BMC Purple
+col.bmc.navy <- rgb(26, 46, 79, maxColorValue = 255)        # BMC Navy Blue
+col.bmc.blue <- rgb(1, 74, 129, maxColorValue = 255)        # BMC Blue
+col.bmc.sky <- rgb(15, 153, 190, maxColorValue = 255)       # BMC Sky Blue
+col.bmc.green.d <- rgb(0, 73, 64, maxColorValue = 255)      # BMC Deep Green
+col.bmc.green.l <- rgb(0, 203, 170, maxColorValue = 255)    # BMC Light Green
+col.bmc.pink <- rgb(239, 37, 95, maxColorValue = 255)       # BMC Pink
 col.sage.purple <- rgb(122, 107, 130, maxColorValue = 255)  # SAGE Purple
 col.sage.red <- rgb(175, 33, 38, maxColorValue = 255)       # SAGE Red
-col.sage.red <- rgb(238, 238, 238, maxColorValue = 255)     # SAGE Gray
+col.sage.gray <- rgb(238, 238, 238, maxColorValue = 255)    # SAGE Gray
 
 
 # fonts ----
